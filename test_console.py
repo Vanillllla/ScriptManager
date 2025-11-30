36467551fd2f2b2cc137fa6c66177c9c29aa9df5
VANILLA-PC <ivan.mavritskiy@gmail.com>
2025-11-30 14:59:07 +0300

    0.12.1(DeepSeek Edition)

--- test_console.py
+++ test_console.py
@@ -1,6 +1,13 @@
+import time
+from tqdm import tqdm
+
 while True:
-    print("Введи текст: ")
+    print("Введи количество ms*100: ")
     t = input()
-    print(t)
-    if t == "exit":
-        break
+    for i in tqdm(range(int(t))):
+        t = (i+123423423428*333)/83746983763984756
+
+    print("Введи количество ms*100 ERROR: ")
+    t = input()
+    for i in tqdm(range(t)):
+        time.sleep(100)
